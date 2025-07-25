d5e589e2d5d509674faabb29ea23fd6ac8ef426f
Tntpot <daniel_ferguson@hotmail.co.uk>
2025-07-25 18:22:01 +0100

    Prettier re-format with updated options

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,16 +1,22 @@
 // .prettierrc.mjs
 /** @type {import("prettier").Config} */
 export default {
-  singleQuote: true,
-  htmlWhitespaceSensitivity: 'ignore',
-  bracketSameLine: true,
-  plugins: ['prettier-plugin-astro'],
-  overrides: [
-    {
-      files: '*.astro',
-      options: {
-        parser: 'astro',
+   singleQuote: true,
+   printWidth: 100, // Maximum line length
+   tabWidth: 3, // Number of spaces per indentation level
+   useTabs: false, // Indent with spaces instead of tabs
+   semi: true, // Print semicolons at the ends of statements
+   trailingComma: 'es5', // Print trailing commas wherever valid in ES5 (objects, arrays, etc.)
+   bracketSpacing: true, // Print spaces between brackets in object literals
+   htmlWhitespaceSensitivity: 'ignore',
+   bracketSameLine: true,
+   plugins: ['prettier-plugin-astro'],
+   overrides: [
+      {
+         files: '*.astro',
+         options: {
+            parser: 'astro',
+         },
       },
-    },
-  ],
+   ],
 };
