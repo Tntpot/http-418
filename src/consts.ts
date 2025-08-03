20a1f744acfc1765d3edb23b3783e241fd61cf43
Tntpot <daniel_ferguson@hotmail.co.uk>
2025-08-03 10:38:35 +0100

    Update: Site Title & Description

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = `Tntpot's Blog`;
-export const SITE_DESCRIPTION = `Welcome to Tntpot's website!`;
+export const SITE_TITLE = `Tntpot`;
+export const SITE_DESCRIPTION = `Tntpot's blog!`;
